37d2e8d4f740b85eb46fb42e80a14d7b4ef1a3f2
Exe-quiel <exe.quiel.developer@outlook.com>
2023-04-21 12:22:37 -0300

    AOS Fixed 2

--- src/components/AtencionYServicio.jsx
+++ src/components/AtencionYServicio.jsx
@@ -15,11 +15,11 @@ const AtencionYServicio = () => {
       <section className=' bg_atencion_servicio grid grid-cols-1 items-center' id='atencion_servicio'>
       <Parallax strength={-400} bgImage={bgImg}>    
         <div className="flex flex-col md:flex-row items-center justify-center gap-5 p-10 md:p-20 text-center">
-          <div className="p-5 bg-white w-4/5 h-auto md:h-60 md:w-2/3 flex flex-col justify-center items-center" data-aos='fade-right' data-aos-offset={-200}>
+          <div className="p-5 bg-white w-4/5 h-auto md:h-60 md:w-2/3 flex flex-col justify-center items-center" data-aos='fade-right' data-aos-offset={200}>
             <h3 className='uppercase text-2xl mb-2 lg:mb-5 font-bold text-blue-700'>Atención de calidad</h3>
             <p>Lograr diferenciación por medio de asesoramiento e inspirar confianza a nuestros clientes para fidelizar la relación y luego ser recomendados. Lo esperamos.</p>
           </div>
-          <div className="p-5 bg-white w-4/5 h-auto md:h-60 md:w-2/3 flex flex-col justify-center items-center" data-aos='fade-left' data-aos-offset={-200}>
+          <div className="p-5 bg-white w-4/5 h-auto md:h-60 md:w-2/3 flex flex-col justify-center items-center" data-aos='fade-left' data-aos-offset={200}>
             <h3 className='uppercase text-2xl mb-2 lg:mb-5 font-bold text-blue-700'>Servicio al cliente</h3>
             <p>Nuestra gran experiencia y trayectoria como gomería nos avalan en el sector. Para nosotros lo más importante es su satisfacción, por eso, realizamos un trabajo eficiente y rápido. Tenemos tecnología avanzada para brindar un servicio de alta calidad. No dude en visitarnos.</p>
           </div>
